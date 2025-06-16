e5ef98ecb832a81799486a4d890027e348b04ae7
Leandro Stevanato Alves <leandro.alves@inmetrics.com.br>
2025-06-16 17:35:08 -0300

    Ano do cartãom é gerado dinamicamente

--- factory/cartao_factory.py
+++ factory/cartao_factory.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from model.cartao import Cartao
 from model.usuario import Usuario
 
@@ -5,9 +6,12 @@ from model.usuario import Usuario
 class CartaoFactory:
     @staticmethod
     def criar(usuario: Usuario) -> Cartao:
+        ano_atual = datetime.now().year
+        # Utiliza o ano do vencimento como sendo 5 anos no futuro
+        ano_futuro = str(ano_atual + 5)[-2:]
         return Cartao(
             "4886123456789012",
             f"{usuario.primeiro_nome} {usuario.ultimo_nome}",
             "567",
-            "122032"
+            f"1220{ano_futuro}"
         )
