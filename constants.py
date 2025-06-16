a2e2094e4f36da5ea8e885571a1d961225a4109c
Leandro Stevanato Alves <leandro.alves@inmetrics.com.br>
2025-06-16 16:24:09 -0300

    Adicionei uma constante para cada endpoint da API

--- constants.py
+++ constants.py
@@ -1,2 +1,4 @@
-API_BASE_URL = "https://www.advantageonlineshopping.com/accountservice/accountrest/api/v1"
+API_BASE_URL = "https://www.advantageonlineshopping.com"
+API_ACCOUNT_BASE_URL = f"{API_BASE_URL}/accountservice/accountrest/api/v1"
+API_ORDER_BASE_URL = f"{API_BASE_URL}/order/api/v1"
 SITE_BASE_URL = "https://advantageonlineshopping.com/#"
